86d1a727b59771652e6fd3758dc7fc8ef4bcde75
DemirKaya7 <74899222+DemirKaya7@users.noreply.github.com>
2021-06-20 13:00:15 -0700

    June 20, 2021
    June 20, 2021

--- _02_basic_gui_apps/_b_my_first_app.py
+++ _02_basic_gui_apps/_b_my_first_app.py
@@ -21,34 +21,36 @@ def create_image(filename, width, height):
 
 # TODO 1) Make a new class and put tk.Tk in the parenthesis, for example:
 #  FirstApp(tk.TK):
-
+class FirstApp(tk.Tk):
     # TODO 2) Make a constructor
-
+    def __init__(self):
         # TODO 3) Call the Tk class's constructor
         #  super().__init__()
-
+        super().__init__()
         # TODO 4) Add a text label and pick a text message to display
-
+        self.label = tk.Label(self, text="My First App", bg='black',
+                              fg='white', font=('arial', 32, 'bold'), relief='solid')
         # TODO 5) Place the label somewhere on your app. You can use either
         #  x and y or relx and rely
-
+        self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
         # TODO 6) Create a member variable for the image using the
         #  create_image function. You can use the image provided in this
         #  folder or another image from the internet
-
+        self.img = create_image('python.png', 200, 200)
         # TODO 7) Create another label and attach to it the image object
         #  from the previous step
-
+        self.label2 = tk.Label(self, image=self.img, bg='yellow',
+                              fg='blue', font=('arial', 32, 'bold'), relief='solid')
         # TODO 8) Place the label somewhere on your app
-
+        self.label2.place(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.6)
 
 # TODO 9) Create an if __name__ == '__main__': block
-
+if __name__ == '__main__':
     # TODO 10) Create an object of your app class
-
+    app = FirstApp()
     # TODO 11) Use the app object to set a title
-
+    app.title = "My First App"
     # TODO 12) Use the app object to set the window dimensions (width x height)
-
+    app.geometry('500x500')
     # TODO 13) Run the app object's mainloop() method
-
+    app.mainloop()
--- _02_basic_gui_apps/_c_photo_quiz.py
+++ _02_basic_gui_apps/_c_photo_quiz.py
@@ -24,36 +24,59 @@ def create_image(filename, width, height):
 #  to use those)
 
 # TODO 1) Create a new tkinter class
-
+class PhotoQuizApp(tk.Tk):
     # TODO 2) Create a constructor
-
+    def __init__(self):
         # TODO 3) call Tk's constructor
-
+        super().__init__()
         # TODO 4) Create a member variable for a label and place it.
         #  You do not need to add any text or images to the label.
-
+        self.label = tk.Label()
+        self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
 # TODO 5) Create an if __name__ == '__main__': block
-
+if __name__ == '__main__':
     # TODO 6) Create an object of the tkinter class
-
+    quiz = PhotoQuizApp()
     # TODO 7) Set the app window width and height using geometry()
-
+    quiz.geometry("500x500")
     # TODO 8) Declare and initialize a score variable
-
+    score = 0
     # TODO 9) Create an image object variable using the create_image function
     #  above and store it in a variable
-
+    image1 = create_image("carrots.jpg", 200, 200)
     # TODO 10) Set the image onto the class's label using the configure method,
     #  for example:
     #  app.photo_label.configure(image=image_object)
-
+    quiz.label.configure(image=image1)
     # TODO 11) Use a pop-up (simpledialog) to ask the user a question
     #  relating to the image and tell them if they get the right answer.
-
+    response1 = simpledialog.askstring(title="Question 1", prompt="What are these?")
     # TODO 12) If the answer is correct, increase the score by 1
-
+    if response1 == "Carrots":
+        score = score + 1
+        messagebox.showinfo(title="Correct", message="You got +1 point")
+    else:
+        messagebox.showinfo(title="Incorrect", message="Sorry, that's wrong")
     # TODO 13) Repeat the steps to show a different photo and ask a different
     #  question
+    image2 = create_image("fossil.jpg", 200, 200)
+    quiz.label.configure(image=image2)
+    response2 = simpledialog.askstring(title="Question 2", prompt="What is this?")
+    if response2 == "A fossil":
+        score = score + 1
+        messagebox.showinfo(title="Correct", message="You got +1 point")
+    else:
+        messagebox.showinfo(title="Incorrect", message="Sorry, that's wrong")
 
+    image3 = create_image("python.png", 200, 200)
+    quiz.label.configure(image=image3)
+    response3 = simpledialog.askstring(title="Question 3", prompt="What colors is this made of?")
+    if response3 == "Blue and yellow":
+        score = score + 1
+        messagebox.showinfo(title="Correct", message="You got +1 point")
+    else:
+        messagebox.showinfo(title="Incorrect", message="Sorry, that's wrong")
+        
     # TODO 14) Display the score to the user after asking the last question
+    messagebox.showinfo(title="Final score", message="Score = " + str(score))
